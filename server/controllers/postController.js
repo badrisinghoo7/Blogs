6fcefe3e7b2c809ae41a4de55c8576cd06d1a263
Badri vishal singh <badri.singh8090@gmail.com>
2023-12-19 19:06:00 +0530

    Creating post controller

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,1 +1,31 @@
 //Post Controller here
+
+// ======> Create A post for user
+//POST : api/posts
+// Protected
+const createPost = async (req, res, next) => {};
+
+// ======> Get single Post
+//POST : api/posts/:id
+// UNprotected
+const getPost = async (req, res, next) => {};
+
+// ======>Get posts by category
+//POST : api/posts/category/:category
+// Unprotected
+const getCategoryPost = async (req, res, next) => {};
+
+// ======> Get users/Authors post
+//POST : api/posts
+// Unprotected
+const getUserPost = async (req, res, next) => {};
+
+// ======> Edit post
+//PATCH : api/posts/:id
+// Protected
+const editPost = async (req, res, next) => {};
+
+// ======> Delete a post
+//DELETE : api/posts/:id
+// Protected
+const deletePost = async (req, res, next) => {};
