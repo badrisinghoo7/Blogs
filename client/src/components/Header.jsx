8055f5d7e60e473f441df00234fdb59da858e2a6
Badri vishal singh <badri.singh8090@gmail.com>
2023-12-20 12:54:07 +0530

    adding some thing for routing

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../images/Blue Gradient Modern Professional Company Zoom Virtual Background.png";
 
 const Header = () => {
   return (
     <div>
-      
+      <nav>
+        <div className="container nav_container">
+          <Link to="/" className="nav_logo">
+            <img src={logo} alt="nav_logo" />
+          </Link>
+          <ul className="nav_menu">
+            <li>
+              <Link to="/profile">Ernest Achiever</Link>
+            </li>
+            <li>
+              <Link to="/create">Create Post</Link>
+            </li>
+            <li>
+              <Link to="/authors">Authors</Link>
+            </li>
+            <li>
+              <Link to="/logout">Logout</Link>
+            </li>
+          </ul>
+          <button className="nav_toggle-btn"></button>
+        </div>
+      </nav>
     </div>
   );
-}
+};
 
 export default Header;
