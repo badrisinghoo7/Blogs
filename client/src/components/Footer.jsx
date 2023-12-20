c63aaa054228305b741dd862303a5a1a28a769be
Badri vishal singh <badri.singh8090@gmail.com>
2023-12-20 16:04:27 +0530

    adding css

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <div>
-      <h1>Footer</h1>
-      
+      <footer>
+        <ul className="footer_categories">
+          <li>
+            <Link to="/posts/category/:Agriculture">Agrculture</Link>
+          </li>
+          <li>
+            <Link to="/posts/category/:Bussiness">Bussiness</Link>
+          </li>
+          <li>
+            <Link to="/posts/category/:Education">Education</Link>
+          </li>
+          <li>
+            <Link to="/posts/category/:Entertainment">Entertainment</Link>
+          </li>
+          <li>
+            <Link to="/posts/category/:Art">Art</Link>
+          </li>
+          <li>
+            <Link to="/posts/category/:Investment">Investment</Link>
+          </li>
+          <li>
+            <Link to="/posts/category/:Uncategorized">Uncategorized</Link>
+          </li>
+          <li>
+            <Link to="/posts/category/:Weather">Weather</Link>
+          </li>
+        </ul>
+        <div className="footer_copyright">
+          <small> All rights have been taken &copy;Copyright Thakur.</small>
+        </div>
+      </footer>
     </div>
   );
-}
+};
 
 export default Footer;
