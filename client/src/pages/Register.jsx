7791db3a52042c68afbc44b229c4ef5c8c2df36f
Badri vishal singh <badri.singh8090@gmail.com>
2023-12-21 02:35:35 +0530

    creating a registered page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 
 const Register = () => {
   return (
-    <div>
-      
-    </div>
+    <section className="register">
+      <div className="container">
+        <h2>Sign Up</h2>
+        <form className='form register_form'>
+          <p className="form_error-message">This is an error message</p>
+          <input type="text" />
+        </form>
+      </div>
+    </section>
   );
 }
 
