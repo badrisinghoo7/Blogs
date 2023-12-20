1a7929359958c7578aabe3b90915509c3607263c
Badri vishal singh <badri.singh8090@gmail.com>
2023-12-21 03:24:12 +0530

    Creating authors page css completed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
           </Link>
           <ul className="nav_menu">
             <li>
-              <Link to="/profile">Ernest Achiever</Link>
+              <Link to="/profile/stdh">Ernest Achiever</Link>
             </li>
             <li>
               <Link to="/create">Create Post</Link>
