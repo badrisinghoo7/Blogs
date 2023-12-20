39cff2c1dbc8af9e6a00eb6ba7c9523d0efc230c
Badri vishal singh <badri.singh8090@gmail.com>
2023-12-21 02:47:59 +0530

    creating a registered page css

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,17 +1,62 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
 
 const Register = () => {
+  const [userData, setuserData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const changeInputHandler = (e) => {
+    setuserData({ ...userData, [e.target.name]: e.target.value });
+  };
   return (
     <section className="register">
       <div className="container">
         <h2>Sign Up</h2>
-        <form className='form register_form'>
+        <form className="form register_form">
           <p className="form_error-message">This is an error message.</p>
-          <input type="text" />
+          <input
+            type="text"
+            placeholder="Full Name"
+            name="name"
+            value={userData.name}
+            onChange={changeInputHandler}
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            name="email"
+            value={userData.email}
+            onChange={changeInputHandler}
+          />
+          <input
+            type="Password"
+            placeholder="Password"
+            name="password"
+            value={userData.password}
+            onChange={changeInputHandler}
+          />
+          <input
+            type="text"
+            placeholder="Confirm Password"
+            name="password2"
+            value={userData.password2}
+            onChange={changeInputHandler}
+          />
+          <button className="btn primary" type="submit">
+            {" "}
+            Submit
+          </button>
         </form>
+        <small>
+          Already have an account ? <Link to="/login">Login</Link>
+        </small>
       </div>
     </section>
   );
-}
+};
 
 export default Register;
