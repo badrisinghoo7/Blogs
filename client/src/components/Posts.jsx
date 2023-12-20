018cf9974059f018f7e14f7af4936f7ef8239f9b
Badri vishal singh <badri.singh8090@gmail.com>
2023-12-21 02:22:06 +0530

    updated some condition rendering

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -1,47 +1,21 @@
 import React, { useState } from "react";
 import PostItem from "./PostItem";
-import pic1 from "../images/blog20.jpg";
-import pic2 from "../images/blog7.jpg";
-import pic3 from "../images/blog10.jpg";
 
-const Dummy_data = [
-  {
-    category: "Agriculture",
-    id: 1,
-    thumbnail: pic1,
-    title: "Composting for Beginners: Turning Waste into Riches",
-    description: "Learn how to create nutrient-rich compost for your garden.",
-    authorID: 1,
-  },
-  {
-    category: "Agriculture",
-    id: 2,
-    thumbnail: pic2,
-    title: "Essential Farm Equipment for Small Holdings",
-    description:
-      "Discover tools and machinery to make your farming tasks easier.",
-    authorID: 1,
-  },
-  {
-    category: "Agriculture",
-    id: 3,
-    thumbnail: pic3,
-    title: "The Buzz on Beekeeping: A Beginner's Guide",
-    description:
-      "Explore the fascinating world of honeybees and learn how to start your own hive.",
-    authorID: 1,
-  },
-];
+import { Dummy_data } from "../data";
 
 const Posts = () => {
   const [posts, setPosts] = useState(Dummy_data);
   return (
     <section className="posts">
-      <div className="container posts_container">
-        {posts.map((item) => {
-          return <PostItem key={item.id} {...item} />;
-        })}
-      </div>
+      {posts.length > 0 ? (
+        <div className="container posts_container">
+          {posts.map((item) => {
+            return <PostItem key={item.id} {...item} />;
+          })}
+        </div>
+      ) : (
+        <h2>No posts Found</h2>
+      )}
     </section>
   );
 };
---
+++ client/src/data.js
@@ -0,0 +1,32 @@
+import pic1 from "./images/blog20.jpg";
+import pic2 from "./images/blog7.jpg";
+import pic3 from "./images/blog12.jpg";
+
+export const Dummy_data = [
+  {
+    category: "Agriculture",
+    id: 1,
+    thumbnail: pic1,
+    title: "Composting for Beginners: Turning Waste into Riches",
+    description: "Learn how to create nutrient-rich compost for your garden.",
+    authorID: 1,
+  },
+  {
+    category: "Agriculture",
+    id: 2,
+    thumbnail: pic2,
+    title: "Essential Farm Equipment for Small Holdings",
+    description:
+      "Discover tools and machinery to make your farming tasks easier.",
+    authorID: 1,
+  },
+  {
+    category: "Agriculture",
+    id: 3,
+    thumbnail: pic3,
+    title: "The Buzz on Beekeeping: A Beginner's Guide",
+    description:
+      "Explore the fascinating world of honeybees and learn how to start your own hive.",
+    authorID: 1,
+  },
+];
