07c04f1e88a446f5408e40be3521b0d7fc0a1e45
Badri vishal singh <badri.singh8090@gmail.com>
2023-12-21 03:07:24 +0530

    Creating authors page

--- client/src/pages/Authors.jsx
+++ client/src/pages/Authors.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import avt1 from "../images/avatar1.jpg"
+import avt2 from "../images/avatar2.jpg"
+import avt3 from "../images/avatar3.jpg"
+import avt4 from "../images/avatar4.jpg"
 
+
+
+const authorData = [
+
+  {id:1,avatar:avt1,name:"Ernest Acheiver", posts:3},
+  {id:2,avatar:avt2,name:" Denot Acheiver", posts:5},
+  {id:3,avatar:avt3,name:" connetErnest ", posts:0},
+  {id:4,avatar:avt4,name:"Erlest clasterr", posts:1}
+]
 const Author = () => {
   return (
     <div>
