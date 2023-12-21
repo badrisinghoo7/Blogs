1c8dfc2453c6191b07da1b940caba11f68b9bbd3
Badri vishal singh <badri.singh8090@gmail.com>
2023-12-22 00:12:58 +0530

    Dashboard section i am working css

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("Uncategorized");
+  const [description, setDescription] = useState("");
+  const [thi, bnaild, setThumbnail] = useState("");
   return (
-    <div>
-      
-    </div>
+    <section className="create-post">
+      <div className="container">
+        <h2>Create Post</h2>
+        <p className="form_error-message">This is an error message</p>
+        <form className="form create-post_form">
+          <input
+            type="text"
+            placeholder="Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            autoFocus
+          />
+          <input
+            type="text"
+            placeholder="Category"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <input type="file" placeholder="Thumbnail" />
+        </form>
+      </div>
+    </section>
   );
-}
+};
 
 export default CreatePost;
