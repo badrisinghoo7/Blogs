551cc4005236612bee14c88964a96b6acdac0990
Badri vishal singh <badri.singh8090@gmail.com>
2023-12-21 18:52:08 +0530

    Dashboard section i am working

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Dummy_data } from "../data";
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
   const [posts, setPosts] = useState(Dummy_data);
@@ -14,6 +15,24 @@ const Dashboard = () => {
                   <div className="dashboard_post-thumbnail">
                     <img src={item.avatar} alt="Name" />
                   </div>
+                  <h5>{item.title}</h5>
+                </div>
+                <div className="dashboard_post-actions">
+                  <Link to={`/posts/${item.id}`} className="btn sm">
+                    View
+                  </Link>
+                  <Link
+                    to={`/posts/${item.id}/edit`}
+                    className="btn sm primary"
+                  >
+                    Edit
+                  </Link>
+                  <Link
+                    to={`/posts/${item.id}/delete`}
+                    className="btn sm danger"
+                  >
+                    Delete
+                  </Link>
                 </div>
               </article>
             );
