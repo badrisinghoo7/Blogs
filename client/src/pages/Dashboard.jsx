cc6d046931dc54724e5cc639670bb82b102e5bd7
Badri vishal singh <badri.singh8090@gmail.com>
2023-12-21 18:49:32 +0530

    Dashboard section i am working

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Dummy_data } from "../data";
 
 const Dashboard = () => {
+  const [posts, setPosts] = useState(Dummy_data);
   return (
-    <div>
-      
-    </div>
+    <section className="dashboard">
+      {posts.length ? (
+        <div className="container dashboard_container">
+          {posts.map((item) => {
+            return (
+              <article className="dashboard_post">
+                <div className="dashboard_post-info">
+                  <div className="dashboard_post-thumbnail">
+                    <img src={item.avatar} alt="Name" />
+                  </div>
+                </div>
+              </article>
+            );
+          })}
+        </div>
+      ) : (
+        <h2>No posts found</h2>
+      )}
+    </section>
   );
-}
+};
 
 export default Dashboard;
